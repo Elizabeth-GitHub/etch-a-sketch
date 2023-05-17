6704f859eb0e38bbdde9342c68a6a2dcec28fd26
Elizabeth <122000137+Elizabeth-GitHub@users.noreply.github.com>
2023-05-17 11:59:50 +0800

    Implement grid resizing and dynamic square size
    Add functionality to dynamically resize the grid based on user input and
    adjusts the size of each square accordingly.
    It also introduces a new button that prompts the user to enter
    the number of squares per side for the new grid.
    The existing grid is removed and replaced with the new grid while
    maintaining the same total space.
    Additionally, the commit updates the CSS to improve the layout and
    center the grid container.
    The changes enhance the user experience and provide a more flexible
    grid system.

--- script.js
+++ script.js
@@ -1,20 +1,54 @@
-const numberContainers = 16;
+const containerMain = document.createElement('div');
+const buttonNewGrid = document.createElement('button');
 const containerGrid = document.createElement('div');
-for (let i = 0; i < numberContainers; i++) {
-    for (let j = 0; j < numberContainers; j++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        containerGrid.appendChild(square);
-        square.addEventListener('mouseenter', handleMouseEnter);
-    }
-}
+const defaultSize = 16;
+createGrid(defaultSize);
 const squares = document.querySelectorAll('.square');
 
+containerMain.setAttribute('id', 'container-main');
 containerGrid.setAttribute('id', 'container-grid');
+buttonNewGrid.classList.add('button');
+buttonNewGrid.textContent = 'NEW GRID';
 
-document.body.appendChild(containerGrid);
+document.body.appendChild(containerMain);
+containerMain.appendChild(buttonNewGrid);
+containerMain.appendChild(containerGrid);
+
+buttonNewGrid.addEventListener('click', handleButtonNewGridClick);
 
 function handleMouseEnter(event) {
     event.target.classList.add('hovered');
 };
 
+function handleButtonNewGridClick() {
+    const newSize = prompt('Enter the number of squares per side for the new grid:');
+
+    if (newSize) {
+        const parsedSize = parseInt(newSize);
+        if (parsedSize > 0) {
+          clearGrid();
+          createGrid(parsedSize);
+        } 
+        else {
+          alert('Invalid input. Please enter a valid number greater than 0.');
+        }
+      }
+};
+
+function clearGrid() {
+    containerGrid.innerHTML = '';
+}
+
+function createGrid(gridSize) {
+    const squareSize = `${100 / gridSize}%`;
+
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.style.flexBasis = squareSize;
+            containerGrid.appendChild(square);
+            square.addEventListener('mouseenter', handleMouseEnter);
+        }
+    }
+}
