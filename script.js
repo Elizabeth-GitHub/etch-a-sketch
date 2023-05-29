a653038eb68e0e8619a95ad8370cc76843cd6c5f
Elizabeth <122000137+Elizabeth-GitHub@users.noreply.github.com>
2023-05-29 12:36:40 +0800

    Refactor code
    Refactor code for better readability

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const containerWorkingArea = document.createElement('div');
 const containerOptions = document.createElement('div'); 
 const containerButtons = document.createElement('div');
 const buttonClear = document.createElement('button');
-const buttonNewGrid = document.createElement('button');
+const buttonChangeGridSize = document.createElement('button');
 const buttonDefaultSize = document.createElement('button');
 const buttonEraser = document.createElement('button');
 const containerToggles = document.createElement('div');
@@ -61,13 +61,15 @@ textHeader.setAttribute('id', 'text-header');
 textHeader.innerText = 'Etch-a-Sketch';
 buttonClear.classList.add('button');
 buttonClear.textContent = 'CLEAR';
-buttonNewGrid.classList.add('button');
-buttonNewGrid.textContent = 'CHANGE GRID SIZE';
+buttonClear.style.display = 'none';
+buttonChangeGridSize.classList.add('button');
+buttonChangeGridSize.textContent = 'CHANGE GRID SIZE';
 buttonDefaultSize.classList.add('button');
 buttonDefaultSize.textContent = 'DEFAULT SIZE';
 buttonDefaultSize.style.display = 'none';
 buttonEraser.classList.add('button');
 buttonEraser.innerText = 'ERASER';
+buttonEraser.style.display = 'none';
 containerToggleRainbow.classList.add('containers', 'container-toggler');
 labelToggleRainbow.classList.add('containers', 'toggle-switch');
 inputToggleRainbow.type = 'checkbox';
@@ -112,12 +114,10 @@ containerHeader.appendChild(textHeader);
 containerMain.appendChild(containerWorkingArea);
 containerWorkingArea.appendChild(containerOptions);
 containerWorkingArea.appendChild(containerGrid);
-/*containerMain.appendChild(containerOptions);
-containerMain.appendChild(containerGrid)*/
 containerOptions.appendChild(containerButtons);
 containerOptions.appendChild(containerToggles);
 containerButtons.appendChild(buttonDefaultSize);
-containerButtons.appendChild(buttonNewGrid);
+containerButtons.appendChild(buttonChangeGridSize);
 containerButtons.appendChild(buttonEraser);
 containerButtons.appendChild(buttonClear);
 containerToggles.appendChild(containerToggleRainbow);
@@ -144,8 +144,12 @@ creator.appendChild(creatorLink);
 containerGrid.addEventListener('mousedown', handleMouseDown);
 containerGrid.addEventListener('mouseup', handleMouseUp);
 buttonClear.addEventListener('click', clearGrid);
-buttonNewGrid.addEventListener('click', handleButtonNewGridClick);
+buttonChangeGridSize.addEventListener('click', handleButtonChangeGridSizeClick);
 buttonDefaultSize.addEventListener('click', function() {
+  if (checkHoveredSquares() && !showChangingAlert()) {
+    return;
+  }
+  
   buttonDefaultSize.style.display = 'none';
   removeGrid();
   createGrid(defaultSize); 
@@ -187,7 +191,7 @@ inputToggleGradient.addEventListener('change', function() {
 
 //
 function eraseSquare(square) {
-  if (square.classList.contains('hovered-rainbow', 'hovered-gradient') ||  square.classList.contains('hovered-gradient')) {
+  if (square.classList.contains('hovered-rainbow') ||  square.classList.contains('hovered-gradient')) {
     square.style.border = '1px solid rgba(221, 160, 221, 1)';
     square.style.backgroundColor = 'white';
     square.style.opacity = 1;
@@ -216,37 +220,84 @@ function handleMouseUp() {
   isPenActive = false;
 }
 
+function drawPlumSquare(squareToPLum) {
+  squareToPLum.classList.add('hovered');
+}
+
+function drawRainbowSquare(squareToRainbow) {
+  const randomRGB = getRandomRGB();
+
+  squareToRainbow.style.backgroundColor = randomRGB;
+  squareToRainbow.style.opacity = 0.2;
+  squareToRainbow.classList.add('hovered-rainbow');
+}
+
+
+/*function askGradientSize() {
+  const gradientSize = prompt('Enter the gradient size (between 0 and 100):\n\n\
+                              The gradient size determines the length of the gradient before it starts repeating. \
+                              A smaller size creates a shorter gradient, while a larger size creates a longer gradient before starting again.');
+
+  if (gradientSize !== null && gradientSize !== '') {
+    const size = parseInt(gradientSize, 10);
+
+    if (Number.isNaN(size) || size < 0 || size > 100) {
+      alert('Invalid gradient size! Please enter a value between 0 and 100.');
+    } 
+  } else {
+    alert('Gradient size not provided! Gradient mode disabled.');
+    this.checked = false;
+    isGradientMode = false;
+    containerToggleRainbow.classList.remove('disabled');
+  }
+}*/
+function drawGradientSquare(squareToGradient) {
+  const maxOpacity = 1;
+  
+  let currentOpacity = previousOpacity;
+
+  squareToGradient.classList.add('hovered-gradient');
+  if (currentOpacity < maxOpacity) {
+    const increment = 0.067
+    const newOpacity = currentOpacity + increment;
+    const lightness = (90 - newOpacity * 80).toFixed(0);
+    const saturation = (20 + newOpacity * 80).toFixed(0);
+        
+    squareToGradient.style.opacity = newOpacity.toString();
+    squareToGradient.style.backgroundColor = `hsl(330, ${saturation}%, ${lightness}%)`;
+    previousOpacity = newOpacity;
+    console.log(previousOpacity);
+  } else {
+    previousOpacity = 0;
+  }
+}
+
+function enableButton(...buttonsToEnable) {
+  buttonsToEnable.forEach(buttonToEnable => {
+    buttonToEnable.style.display = 'flex';
+  });
+}
+
+function disableButton(...buttonsToDisable) {
+  buttonsToDisable.forEach(buttonToDisable => {
+    buttonToDisable.style.display = 'none';
+  });
+}
+
+
+
 function handleMouseEnter(event) {
   if (isPenActive) {
     const square = event.target;
 
     if (isRainbowMode) {
-      const randomRGB = getRandomRGB();
-
-      square.style.backgroundColor = randomRGB;
-      square.style.opacity = 0.2;
-      square.classList.add('hovered-rainbow');
+      drawRainbowSquare(square);
     } else if (isGradientMode) {
-      const maxOpacity = 1;
-      let currentOpacity = previousOpacity;
-
-      square.classList.add('hovered-gradient');
-      if (currentOpacity < maxOpacity) {
-        const increment = 0.067
-        const newOpacity = currentOpacity + increment;
-        const lightness = (90 - newOpacity * 80).toFixed(0);
-        const saturation = (20 + newOpacity * 80).toFixed(0);
-        
-        square.style.opacity = newOpacity.toString();
-        square.style.backgroundColor = `hsl(330, ${saturation}%, ${lightness}%)`;
-        previousOpacity = newOpacity;
-        console.log(previousOpacity);
-        } else {
-          previousOpacity = 0;
-        }
-      } else {
-      square.classList.add('hovered');
+      drawGradientSquare(square);
+    } else {
+      drawPlumSquare(square);
     }
+    enableButton(buttonClear, buttonEraser);
   }
 }
 
@@ -254,69 +305,106 @@ function toggleEraserMode() {
   isEraserActive = !isEraserActive;
 
   if (isEraserActive) {
-    containerGrid.classList.add('cursor-eraser');
     buttonEraser.classList.add('active');
+    containerGrid.classList.add('cursor-eraser');
   } else {
     buttonEraser.classList.remove('active');
     containerGrid.classList.remove('cursor-eraser');
   }
 }
 
-
 function getRandomRGB() {
-  const r = Math.floor(Math.random() * 256);
-  const g = Math.floor(Math.random() * 256);
-  const b = Math.floor(Math.random() * 256);
-  return `rgb(${r},${g},${b})`;
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  return `rgb(${red},${green},${blue})`;
 }
 
 function removeGrid() {
     containerGrid.innerHTML = '';
 }
 
-function clearGrid() {
-  const squaresHovered = document.querySelectorAll('.square.hovered, .square.hovered-rainbow, .square.hovered-gradient');
+function findHoveredSquares() {
+  return document.querySelectorAll('.square[class*="hovered"]');
+}
 
-  squaresHovered.forEach((squareHovered) => {
-    eraseSquare(squareHovered);
-});
-  isPenActive = false; 
+function clearGrid() {
+  if (showChangingAlert()) {
+    const areaHovered = findHoveredSquares();
+
+    areaHovered.forEach((elementHovered) => {
+      eraseSquare(elementHovered);
+  });
+    isPenActive = false; 
+    disableButton(buttonClear, buttonEraser);
+  } else {
+    return
+  }
 }
 
 function createGrid(gridSize) {
-    const squareSize = `${100 / gridSize}%`;
+  const squareSize = `${100 / gridSize}%`;
 
-    for (let i = 0; i < gridSize; i++) {
-        for (let j = 0; j < gridSize; j++) {
-            const square = document.createElement('div');
-            square.classList.add('square', 'show-grid');
-            square.style.flexBasis = squareSize;
-            containerGrid.appendChild(square);
-            square.addEventListener('mouseenter', handleMouseEnter);
-        }
-    }
+  for (let i = 0; i < gridSize; i++) {
+      for (let j = 0; j < gridSize; j++) {
+          const squareToCreate = document.createElement('div');
+
+          squareToCreate.classList.add('square', 'show-grid');
+          squareToCreate.style.flexBasis = squareSize;
+          containerGrid.appendChild(squareToCreate);
+          squareToCreate.addEventListener('mouseenter', handleMouseEnter);
+      }
+  }
 }
 
-function handleButtonNewGridClick() {
-    let parsedSize = NaN;
-  
-    while (isNaN(parsedSize) || parsedSize <= 0 || parsedSize > 100) {
-      const newSize = prompt('Enter the number of squares per side for the new grid (between 1 and 100):');
-  
-      if (newSize === null) {
-        return; // Exit the function if the user clicks "Cancel"
-      }
-  
-      parsedSize = parseInt(newSize);
-  
-      if (parsedSize <= 0) {
-        alert('Number of squares should be at least 1!');
-      } else if (parsedSize > 100) {
-        alert('Maximum number of squares is 100!');
-      }
-    }
-  
-    removeGrid();
-    createGrid(parsedSize);
-    buttonDefaultSize.style.display = 'flex';
+function checkHoveredSquares() {
+  const hoveredSquares  = findHoveredSquares();
+
+  if (hoveredSquares.length > 0) {
+    return true;
+  } else {
+    return false;
   }
+}
+
+function handleButtonChangeGridSizeClick() {
+
+  if (checkHoveredSquares()) {
+    if (showChangingAlert()) { // A user agreed to reset the current drawing
+      changeGridSize(); 
+    } else {
+      return;
+    }
+  } else { 
+    changeGridSize(); 
+  }
+}
+
+function showChangingAlert() {
+  const confirmed = confirm('The current drawing will not be saved. Are you sure you want to proceed?');
+  return confirmed;
+}
+
+function changeGridSize(){
+  let parsedSize = NaN;
+  
+  while (isNaN(parsedSize) || parsedSize <= 0 || parsedSize > 100) {
+    const newSize = prompt('Enter the number of squares per side for the new grid (between 1 and 100):');
+
+    if (newSize === null) {
+      return; // Exit the function if the user clicks "Cancel"
+    }
+
+    parsedSize = parseInt(newSize);
+
+    if (parsedSize <= 0) {
+      alert('Number of squares should be at least 1!');
+    } else if (parsedSize > 100) {
+      alert('Maximum number of squares is 100!');
+    }
+  }
+
+  removeGrid();
+  createGrid(parsedSize);
+  buttonDefaultSize.style.display = 'flex';
+} 
